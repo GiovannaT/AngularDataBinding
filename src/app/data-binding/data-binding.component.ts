8c9be51b25654468fc3f1a9047f869d4bb576b3f
Giovanna Trigueiro <giovanna.trigueiro@gmail.com>
2021-12-28 11:36:42 -0300

    DB, 2way, in-output

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
+  styles:[`
+    .highlight{
+      background-color: yellow;
+      font-weight: bold;
+    }
+  `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem = 'https://loremflickr.com/320/240';
+  isMouseOver : boolean = false;
+
+  nomeDoCurso: string = 'Angular';
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  
+  valorInicial = 15;
   
   getValor()
   {
@@ -18,6 +33,22 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado!')
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
